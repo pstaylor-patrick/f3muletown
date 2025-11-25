cf842e7bcdf78c813b85d65460392d4d9c4b4e66
Patrick Taylor <1963845+pstaylor-patrick@users.noreply.github.com>
2025-11-25 09:05:08 -0600

    Refactor stats page to use shared redirect function

--- apps/stats/app/page.tsx
+++ apps/stats/app/page.tsx
@@ -1,8 +1,6 @@
 import { redirect } from "next/navigation";
+import { getStatsRedirectUrl } from "@f3muletown/redirects";
 
 export default function Home() {
-  const year = new Date().getFullYear();
-  const target = `https://www.yourfullstack.com/apps/f3fw/ytd.php?year=${year}&location=f3muletown`;
-
-  redirect(target);
+  redirect(getStatsRedirectUrl());
 }
